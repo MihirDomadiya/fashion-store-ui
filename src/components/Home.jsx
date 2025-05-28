3d89d319b9249010640fcafcf405d00cb2fd2b00
MihirDomadiya <mihirdomadiya55@gmail.com>
2025-05-28 22:12:05 +0530

    MD | Update landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,17 +14,27 @@ export default function Home() {
                 <Search className='search-icon' />
                 <input type="text" placeholder='Search' className='search-bar col-12 col-xl-4 col-lg-6 col-md-8 text-right' />
             </div>
+            <div className="flex justify-content-between mb-[42px]">
+                <div className="fashion-container flex flex-col w-[40%] justify-content-between h-[452px]">
+                    <div>
+                        <h1 className='fashion-title mb-0'>NEW  <br />COLLECTION</h1>
+                        <h1 className='fashion-sub-title mb-0'>Summer <br /> 2025</h1>
+                    </div>
 
-            <div className="fashion-container">
-                <h1 className='fashion-title mb-0'>NEW  <br />COLLECTION</h1>
-                <h1 className='fashion-sub-title mb-0'>Summer <br /> 2025</h1>
+                    <div className="d-flex">
+                        <button className='w-50 shop-btn d-flex align-items-center justify-between col-12 col-xl-3 col-lg-5 col-md-7'>Go To Shop <img src="/assets/images/right-arrow.png" alt="" /></button>
+                        <button className='home-arrow-lft'><ChevronLeft /></button>
+                        <button className='home-arrow-rt'><ChevronRight /></button>
+                    </div>
+                </div>
 
-                <div className="d-flex">
-                    <button className='shop-btn d-flex align-items-center justify-between col-12 col-xl-3 col-lg-5 col-md-7'>Go To Shop <img src="/assets/images/right-arrow.png" alt="" /></button>
-                    <button className='home-arrow-lft'><ChevronLeft /></button>
-                    <button className='home-arrow-rt'><ChevronRight /></button>
+                <div className="flex w-[60%]">
+                    <img src="/assets/images/Rectangle 12.png" alt="" className='cloth-img' />
+                    <img src="/assets/images/Rectangle 13.png" alt="" className='' />
                 </div>
             </div>
         </div>
     )
 }
+
+
